5fd5f4d13d1ef7b6e07e0effba6ff196c32bdd8a
locdi <minhphucetc1@gmail.com>
2023-08-17 01:05:14 +0700

    upgrade

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const invBtn = document.querySelector('.inv-js')
 const getModal = document.querySelector('.modal-js')
 const closeModal = document.querySelector('.close-js')
+const childrenModal = document.querySelector('.modal-jss')
 function showInvite() {
     getModal.classList.add('open')
 }
 function closeInvite(){
     getModal.classList.remove('open')
 }
+
     invBtn.addEventListener('click', showInvite)
     closeModal.addEventListener('click', closeInvite)
+function closedModal(){
+    getModal.classList.remove('open')
+}
 
+    getModal.addEventListener('click', closedModal)
+    childrenModal.onclick = function(a){
+       a.stopPropagation()
+    }
